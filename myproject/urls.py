2be5d9a2a7230df7dd86a27f7d06bef00e63b904
sherwin-roger0 <sherwinroger02@gmail.com>
2024-05-08 17:58:08 +0530

    updated

--- myproject/urls.py
+++ myproject/urls.py
@@ -6,15 +6,19 @@ from django.db import connection
 import json
 from django.views.decorators.csrf import csrf_exempt
 import os
+from llama_index.core import StorageContext, load_index_from_storage
+
+os.environ["OPENAI_API_KEY"] = ""    
+
+storage_context = StorageContext.from_defaults(persist_dir="myproject/AI")
+index = load_index_from_storage(storage_context)
+query_engine = index.as_query_engine() 
 
 @csrf_exempt
 def niaa_view(request):
     os.environ["OPENAI_API_KEY"] = "API-KEY"
     if request.method == 'POST':
-        from llama_index import StorageContext, load_index_from_storage
-        storage_context = StorageContext.from_defaults(persist_dir="myproject/AI")
-        index = load_index_from_storage(storage_context)
-        query_engine = index.as_query_engine() 
+        
         query = request.body.decode('utf-8')
         response = query_engine.query(query)
 
